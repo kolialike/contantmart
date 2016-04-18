8d15b09c3fdd2c6765a0ffcf524950ec2130c703
kolialike <kolialike@gmail.com>
2016-04-18 10:42:58 +0300

    commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,17 @@
 jQuery(function($){
-  $('.flexslider').flexslider({
-    animation: "slide"
+  var menuBurger = $(".menu-burger");
+  var body = $("body");
+  menuBurger.on('click', function(event) {
+  	event.preventDefault();
+  	body.toggleClass('mobile-menu-open');
+  });
+  var mobileMenu = $(".mobile-menu");
+  mobileMenu.on('click', function(event) {
+  	event.preventDefault();
+  	body.removeClass("mobile-menu-open");
+  });
+  var mobileMenuItem = $(".mobile-menu-item");
+  mobileMenuItem.on('click', function(event) {
+  	event.stopPropagation();
   });
 });
